d255cd76651f460b9fa5109bc2607dc0493d1341
Sahil Tandon <standon196@gmail.com>
2016-04-03 17:03:52 +0530

    Added Share Weather Option in Details Fragement

---
+++ app/src/main/java/com/standon/sahil/sunshine/MainActivity.java
@@ -0,0 +1,74 @@
+package com.standon.sahil.sunshine;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent clickIntent = new Intent(getApplicationContext(),SettingsActivity.class);
+            startActivity(clickIntent);
+            return true;
+        }
+        if(id == R.id.action_in_map){
+            openPrefferedLocationInMap();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openPrefferedLocationInMap(){
+        SharedPreferences sharedprefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedprefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q",location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else{
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no map application installed ");
+        }
+    }
+}
+
